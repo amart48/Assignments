a290acc6d014962a876b57e661de0252a56b823b
mart2164 <mart2164@purdue.edu>
2024-03-04 16:41:47 -0500

    Made changes to the digits function

--- CNIT155_Assignment07.py
+++ CNIT155_Assignment07.py
@@ -20,15 +20,17 @@ def factorial(N):
     factorial = math.factorial(N)
     return factorial
 
-def maximumNo(N1,N2,N3):
-    if N1 > N2 and N3:
+def maximumNo(N1, N2, N3):
+    if N1 > N2 and N1 > N3:
         return N1
-    elif N2 > N1 and N3:
+    elif N2 > N1 and N2 > N3:
         return N2
-    elif N3 > N1 and N2:
+    elif N3 > N1 and N3 > N2:
         return N3
 
-#def digits():
+def digits(N):
+    numberResult = len(N)
+    return numberResult
 
 def main():
     flag = True # Set the flag to true to repeat the main function 
@@ -48,25 +50,26 @@ def main():
             factorialResult = factorial(N)
             print(f"{N} ! = {factorialResult}")
 
-
         elif user_input == 2:
-            print("2. Find the maximum")
+            print("\n2. Find the maximum")
             N1 = float(input("Please enter the 1st number: "))
             N2 = float(input("Please enter the 2nd number: "))
             N3 = float(input("Please enter the 3rd number: "))
-            result = maximumNo(N1,N2,N3)
-            print(f"The greatest number among the three numbers: {int(result)}")
+            maxResult = maximumNo(N1,N2,N3)
+            print(f"The greatest number among the three numbers: {int(maxResult)}")
 
         elif user_input == 3:
-            print("3. Find the number of digits")
-            N = int(input("Enter a natural number for N: "))
+            print("\n3. Find the number of digits")
+            N = input("Enter a natural number for N: ")
+            numberResult = digits(N)
+            print(f"The number of digits in {N} is {numberResult}")
 
         elif user_input == 4:
             flag = False # Sets the flag to false to end the program
             print("Bye!")
 
         else:
-            print("Invalid option! Enter a number between 1 to 4")
+            print("Invalid option! Enter a number between 1 and 4")
 
 
 displayMyInfo() # Call the displayMyInfo function
