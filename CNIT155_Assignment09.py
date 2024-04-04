baba879e15ba3b167852093a1ff721fa21462501
mart2164 <mart2164@purdue.edu>
2024-04-04 14:53:34 -0400

    Made Changes

--- CNIT155_Assignment09.py
+++ CNIT155_Assignment09.py
@@ -12,75 +12,76 @@
 
 def main():
     try:
-        with open('input.txt', 'r') as fileInput:
-            with open('output.txt', 'w') as fileOutput:
-                
-                Names = []
-                Scores = []
-                
-                # Loop to capitalize first and last name of each line in the input file
-                for line in fileInput:
-                    # Strip removes empty space 
-                    # Split the line into seperate words 
-                    temp = line.strip().split()
-                    # Get the first name and capitalize it
-                    firstName = temp[0].title()
-                    # Get the last name and capitalize it
-                    lastName = temp[1].title()
-                    # Concatenate the first and last name and add to Names list
-                    Names.append(firstName + " " + lastName)
-                    # Adding the score of the student and turn it into a float
-                    Scores.append(float(temp[2]))
-                
-                print("Printing every content in the file...\n", Names)
-                print(Scores)
-                
-                # Finding the max score in Scores
-                maxScore = max(Scores)
-                
-                # Initializing maxScoreStudents
-                maxScoreStudents = []
-                # Loop to find students who had the max score and adding them to maxScoreStudents
-                for i, score in enumerate(Scores):
-                    if score == maxScore:
-                        maxScoreStudents.append(Names[i])
-                
-                # Writing in the output file what the the max score is 
-                # and what student had the max score
-                fileOutput.write(f"\nMaximum score is: {maxScore}\n")
-                for student in maxScoreStudents:
-                    fileOutput.write(f"{student} {maxScore} \n")
-                
-                print("Scores have been updated...")
-                
-                fileOutput.write("\nUpdated score(s) with letter grade:\n")
-                for i in range(len(Scores)):
-                    # If the students has a score lower than a 4.0  
-                    # then they will have .1 added to their grade
-                    if Scores[i] < 4.0:
-                        Scores[i] += 0.1
-                    # Second part of the loop to add the corresponding grade to the student
-                    if Scores[i] >= 3.7:
-                        grade = 'A'
-                    elif Scores[i] >= 3.5:
-                        grade = 'B'
-                    elif Scores[i] >= 3.0:
-                        grade = 'C'
-                    elif Scores[i] >= 2.5:
-                        grade = 'D'
-                    else:
-                        grade = 'F'
-                    fileOutput.write("{} {:.2f}, {}\n".format(Names[i], Scores[i], grade))
+        fileInput = open('input.txt', 'r')
+        fileOutput = open('output.txt', 'w')
+
+        # Creating the Names list and Scores list               
+        Names = []
+        Scores = []
+            
+        # Loop to capitalize first and last name of each line in the input file
+        for line in fileInput:
+            # Strip removes empty space 
+            # Split the line into seperate words 
+            temp = line.strip().split()
+            # Get the first name and capitalize it
+            firstName = temp[0].title()
+            # Get the last name and capitalize it
+            lastName = temp[1].title()
+            # Concatenate the first and last name and add to Names list
+            Names.append(firstName + " " + lastName)
+            # Adding the score of the student and turn it into a float
+            Scores.append(float(temp[2]))
+        
+        print("Printing every content in the file...\n", Names)
+        print(Scores)
+        
+        # Finding the max score in Scores
+        maxScore = max(Scores)
+        
+        # Creating the maxScoreStudents list
+        maxScoreStudents = []
+        # Loop to find students who had the max score and adding them to maxScoreStudents
+        for i, score in enumerate(Scores):
+            if score == maxScore:
+                maxScoreStudents.append(Names[i])
+        
+        # Writing in the output file what the the max score is 
+        # and which students had the max score
+        fileOutput.write(f"\nMaximum score is: {maxScore}\n")
+        for student in maxScoreStudents:
+            fileOutput.write(f"{student} {maxScore} \n")
+        
+        # Assignment says to add but is not in the desired outputs
+        # print("Scores have been updated...")
+        
+        fileOutput.write("\nUpdated score(s) with letter grade:\n")
+        for i in range(len(Scores)):
+            # If the students has a score lower than a 4.0  
+            # then they will have .1 added to their grade
+            if Scores[i] < 4.0:
+                Scores[i] += 0.1
+            # Second part of the loop to add the grade to the student
+            if Scores[i] >= 3.7:
+                grade = 'A'
+            elif Scores[i] >= 3.5:
+                grade = 'B'
+            elif Scores[i] >= 3.0:
+                grade = 'C'
+            elif Scores[i] >= 2.5:
+                grade = 'D'
+            else:
+                grade = 'F'
+            # Formatting for the names, scores and grade in the output.txt
+            fileOutput.write("{} {:.2f}, {}\n".format(Names[i], Scores[i], grade))
+        
+        fileInput.close()
+        fileOutput.close()
 
     # Throw an error code instead of ending the program            
     except FileNotFoundError:
         print("The program failed to open the file. Make sure of the followings:")
         print("\t\tThe file to read is located in the same folder where this program is!")
         print("\t\tThe file's name is splled correctly!")
-
-    # Closing the files
-    finally:
-        fileInput.close()
-        fileOutput.close()
     
 main()
